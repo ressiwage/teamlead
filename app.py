b69b2f3a42b382c62bfcb1738eb5ee5262bd5089
1 <ressivage@ya.ru>
2022-08-14 00:48:26 +0500

    1

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy #
+import random, os
+
+app=Flask(__name__)
+            
+@app.route('/', methods=['GET', 'POST'])
+def index():            
+    return render_template("index.html")
+
+@app.route('/<string:defined_page>')
+def custom(defined_page):
+    return redirect("/")
+
+if __name__ == '__main__':
+    app.run(debug = True)
