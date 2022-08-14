1b95d9c95b1d497982d9839dd6a51783d00680b1
1 <ressivage@ya.ru>
2022-08-14 05:45:50 +0500

    1

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy #
+from flask import Flask, render_template,  request, redirect
 import random, os
 
 app=Flask(__name__)
